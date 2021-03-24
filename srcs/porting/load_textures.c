04aa767dd90a1b2c4789e6e0c987e37b92db6de2
kafkalainen <joonas.hj.nivala@gmail.com>
2021-03-24 17:26:12 +0200

    Added texture loading helper functions.

--- srcs/porting/load_textures.c
+++ srcs/porting/load_textures.c
@@ -6,20 +6,30 @@
 /*   By: jnivala <jnivala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/24 16:53:07 by jnivala           #+#    #+#             */
-/*   Updated: 2021/03/24 17:10:34 by jnivala          ###   ########.fr       */
+/*   Updated: 2021/03/24 17:24:02 by jnivala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../wolf3d.h"
 
-int		load_textures(SDL_Surface **textures, int nbr_of_textures)
+static int	load_texture(SDL_Surface *texture, char *path)
+{
+	texture = SDL_LoadBMP(path);
+	if (!(texture))
+		error_output("ERROR: Texture not found.");
+	return (0);
+}
+
+
+int			load_textures(SDL_Surface **textures, int nbr_of_textures)
 {
 	textures = malloc(sizeof(SDL_Surface*) * (nbr_of_textures + 1));
-	if (!(textures[0] = SDL_LoadBMP("resources/wall_bricks.bmp")))
-		error_output("ERROR: Texture not found.");
-	textures[1] = SDL_LoadBMP("resources/wall_bricks.bmp");
-	textures[2] = SDL_LoadBMP("resources/wall_bricks.bmp");
-	textures[3] = SDL_LoadBMP("resources/wall_bricks.bmp");
-	textures[4] = SDL_LoadBMP("resources/floor1.bmp");
-	textures[5] = SDL_LoadBMP("resources/tiles1.bmp");
+	load_texture(textures[0], "resources/wall_bricks.bmp");
+	load_texture(textures[1], "resources/wall_bricks.bmp");
+	load_texture(textures[2], "resources/wall_bricks.bmp");
+	load_texture(textures[3], "resources/wall_bricks.bmp");
+	load_texture(textures[4], "resources/wall_bricks.bmp");
+	load_texture(textures[5], "resources/floor1.bmp");
+	load_texture(textures[6], "resources/tiles1.bmp");
+	return (0);
 }
