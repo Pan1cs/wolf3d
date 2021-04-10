22cdcbde4668062c599fae63358d06e335963890
kafkalainen <joonas.hj.nivala@gmail.com>
2021-04-10 15:41:45 +0300

    Limited range to 0..127 in for texel function, so that it won't go over the pixel range.

--- srcs/update_screen/draw_tex_pixel.c
+++ srcs/update_screen/draw_tex_pixel.c
@@ -6,7 +6,7 @@
 /*   By: jnivala <jnivala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 11:27:22 by jnivala           #+#    #+#             */
-/*   Updated: 2021/04/09 08:35:25 by jnivala          ###   ########.fr       */
+/*   Updated: 2021/04/10 15:37:28 by jnivala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void		draw_tex_pixel(t_texture *tex, t_xyz texel,
 {
 	Uint32		color;
 
-	color = get_texel(tex, texel.x * tex->w, texel.y * tex->h);
+	color = get_texel(tex, texel.x * (tex->w - 1), texel.y * (tex->h - 1));
 	color = colour_scale(color, frame->left.wall->wall_facing);
 	put_pixel(frame->draw_surf, pixel.x, pixel.y, color);
 }
