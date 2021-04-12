b48e1284c7b3aac1a91801d381fc3f5817e7f2d4
kafkalainen <joonas.hj.nivala@gmail.com>
2021-04-12 13:44:42 +0300

    Added vec2_angle check that if the angle is closer to player, and adding one before recursion inside the portal.

--- srcs/raycaster/setup_frame.c
+++ srcs/raycaster/setup_frame.c
@@ -6,7 +6,7 @@
 /*   By: jnivala <jnivala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/05 10:26:00 by jnivala           #+#    #+#             */
-/*   Updated: 2021/03/25 10:33:01 by jnivala          ###   ########.fr       */
+/*   Updated: 2021/04/12 13:40:43 by jnivala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void		setup_frame(t_frame *frame, t_frame *new_frame,
 	if ((frame->offset - current_pxl) < 0)
 		new_frame->max_fov = 0.0f;
 	else
-		new_frame->max_fov = frame->offset - current_pxl;
+		new_frame->max_fov = frame->offset - current_pxl + 1;
 	new_frame->offset = frame->offset;
 	new_frame->draw_surf = frame->draw_surf;
 	new_frame->pxl_offset = frame->pxl_offset;
